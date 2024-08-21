e71647b02735354b434d9b75ed6c2b9738d6a947
dimitar-georgiev-blankfactor <dimitar.georgiev@blankfactor.com>
2024-08-21 11:16:39 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -86,8 +86,8 @@ function populateTableSvetoslav(data) {
     const fetchGoogleCalendarData = async () => {
     let datePattern = handleDropdownChange();
     alert("data returned from method datePattern()"+datePattern);
-    
-    const response = await fetch("https://www.googleapis.com/calendar/v3/calendars/hms@blankfactor.com/events?q=HomeOffice - Svetoslav Zlatev&timeMin=2024-07-31T00:00:00.00Z&timeMax=2024-08-31T00:00:00.00Z&showDeleted=false", {
+
+    const response = await fetch("https://www.googleapis.com/calendar/v3/calendars/hms@blankfactor.com/events?q=HomeOffice - Svetoslav Zlatev&timeMin=" + datePattern + "&showDeleted=false", {
       method: "GET",
       headers: {
           'Authorization': 'Bearer ' + session.provider_token // Access token for Google
